3c878d1d7b221ae3fa6d4809a5d7546c0c2d4215
alxmart <alxmartinez@gmail.com>
2023-01-28 15:10:39 -0300

    Set image according to randomInt.

--- app/src/main/java/com/luizafmartinez/diceroller/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/diceroller/MainActivity.kt
@@ -28,6 +28,15 @@ class MainActivity : AppCompatActivity() {
         val randomInt = Random.nextInt(6) + 1
         //resultText.text = randomInt.toString()
         val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
 
 
     }
