9319cc2e0426350532a9329bf1560c02b9e93ed5
alxmart <alxmartinez@gmail.com>
2023-01-28 14:10:11 -0300

    Added a setOnclickListener showing a Toast message.

--- app/src/main/java/com/luizafmartinez/diceroller/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.luizafmartinez.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Button Clicked",
+                Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
     }
 }
